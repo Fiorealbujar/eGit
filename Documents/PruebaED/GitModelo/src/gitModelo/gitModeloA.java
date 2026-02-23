56e9a7119e9ec8a0e9ab38b93524846f99881555
fiorella <fiorerut8@hotmail.com>
2026-02-23 11:48:36 +0100

    Segundo commit

--- Documents/PruebaED/GitModelo/src/gitModelo/gitModeloA.java
+++ Documents/PruebaED/GitModelo/src/gitModelo/gitModeloA.java
@@ -4,7 +4,8 @@ public class gitModeloA {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		System.out.println("Hola a tod@s");
+		System.out.println("Hola a Tod@s");
+		
 	}
 
 }
