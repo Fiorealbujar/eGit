08c3687b54564d78228401b10b8e2504764426ca
fiorella <fiorerut8@hotmail.com>
2026-02-23 09:55:52 +0100

    Primer commit

---
+++ GitModeloA/gitModeloA.java
@@ -0,0 +1,10 @@
+package modeloA;
+
+public class gitModeloA {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+        System.out.println("Hola a tod@s");
+	}
+
+}
