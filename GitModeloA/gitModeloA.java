cd76cb29fc656fff993b6850c63b7cfbeef4e78c
Fiorella Albujar <115477583+Fiorealbujar@users.noreply.github.com>
2026-02-23 12:07:21 +0100

    Saludo desde remoto

--- GitModeloA/gitModeloA.java
+++ GitModeloA/gitModeloA.java
@@ -5,6 +5,7 @@ public class gitModeloA {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
         System.out.println("Hola a tod@s");
+		System.out.println("Hola desde remoto");
 	}
 
 }
